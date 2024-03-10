1446b962e19aa7ea6477811f849346de82f96920
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-10 18:19:55 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -28,27 +28,30 @@ const user = {
 //     console.log(this.username);
 // }
 
-// arrow function :
+// arrow function syntax : () => {}
 const chai =  () => {
     let username = "hitesh"
     console.log(this);
 }
 // chai()
 
-// const addTwo = (num1, num2) => {
+// const addTwo = (num1, num2) => {     // basic arrow function
 //     return num1 + num2
 // }
 
-// const addTwo = (num1, num2) =>  num1 + num2
+
+// implicit return : no need for return keyword since its a single line of code
+// const addTwo = (num1, num2) =>  num1 + num2      
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
+// Attention : above after arrow agar curly braces use kr liya instead of parenthesis tab return keyword is needed
+// here "( num1 + num2 )"
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"})      // aboject return
 
 
 console.log(addTwo(3, 4))
 
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+// function () {}   // simple function
+// () => {}         // arrow function
+// () => ()         // implicit return in arrow function
