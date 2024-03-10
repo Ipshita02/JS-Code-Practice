4a7c4fd83956f2e0b7ad14ecc7240151b7c1e096
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-10 18:20:08 +0000

    for practice

---
+++ 03_basics/arrowFunc_practice.js
@@ -0,0 +1,19 @@
+// arrow function syntax : () => {}
+
+const addNums = (num1, num2) => num1 + num2
+
+console.log(addNums(10, 20));
+
+
+// object return 
+const obj = () => ({name: "ipshita", age: 21})
+
+console.log(obj());
+
+
+const sayHello = (name) => {
+    return `Hello ${name}!!`
+}
+
+console.log(sayHello("ipshita"))
+
