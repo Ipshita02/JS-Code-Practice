6fdea2580384963340bd09af8b5b51e4de32d9e7
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-12 18:03:50 +0000

    arrow fn

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,7 +4,7 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        // this keyword is used to refer current context or current scope or current values of the variable
+        // this keyword is used to refer current context. or current scope or current values of the variable
         console.log(this);
     }
 
@@ -14,11 +14,12 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);    // in browser the global object is window
+// console.log(this);    // but in browser the global object/scope is window 
 
 // function chai(){
+//     console.log(this)
 //     let username = "hitesh"
-//     console.log(this.username);      // it is worrking in objects. Functions ke andar aakr 'this' use nhi kr paa rhe h
+//     console.log(this.username);      // 'this' is working in objects. Inside functions 'this' not working
 // }
 
 // chai()
@@ -31,7 +32,7 @@ const user = {
 // arrow function syntax : () => {}
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this.username);
 }
 // chai()
 
@@ -50,8 +51,12 @@ const chai =  () => {
 const addTwo = (num1, num2) => ({username: "hitesh"})      // aboject return
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))        ~~~
 
 // function () {}   // simple function
 // () => {}         // arrow function
 // () => ()         // implicit return in arrow function
+// we can hold these functions inside a variable
+
+// we can use "this" inside arrow fn. they point to their lexical scope.
+// scope where code/block are written is called lexical scope of arrow fn.
