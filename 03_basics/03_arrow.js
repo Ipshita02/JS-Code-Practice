17b088b0147c863d46aef24c536dae7c67f5bc23
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-15 15:47:00 +0000

    arrow fn

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -14,7 +14,7 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);    // but in browser the global object/scope is window 
+// console.log(this);    // but in browser the global object is window
 
 // function chai(){
 //     console.log(this)
@@ -51,12 +51,9 @@ const chai =  () => {
 const addTwo = (num1, num2) => ({username: "hitesh"})      // aboject return
 
 
-// console.log(addTwo(3, 4))        ~~~
+console.log(addTwo(3, 4))
 
 // function () {}   // simple function
 // () => {}         // arrow function
 // () => ()         // implicit return in arrow function
 // we can hold these functions inside a variable
-
-// we can use "this" inside arrow fn. they point to their lexical scope.
-// scope where code/block are written is called lexical scope of arrow fn.
