00391efdfd75f3c0fff2de73d795b236367c5806
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-15 15:47:35 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// shortcut : shift + alt + down-arrow :: to copy & paste a bunch of lines
+const Month = 4
+switch (Month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("Febuary");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 4:
+        console.log("April");        
+        break;
+    case 5:
+        console.log("May");        
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
