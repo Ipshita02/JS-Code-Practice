3a753b6619c926a37d867ec57c6dcc8831cbac91
Ipshita02 <72136711+Ipshita02@users.noreply.github.com>
2024-03-20 07:32:59 +0000

    map method

---
+++ 05_iterations/07_seven.js
@@ -0,0 +1,17 @@
+// map() method :
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+let newNums = myNumers             // chaining method 
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+// console.log(newNums);
+
+
+newNums = myNumers.forEach ((nums) => {         // trying with forEach method
+    console.log(nums + 10)
+})
